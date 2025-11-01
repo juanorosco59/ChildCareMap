e7c0ca283676af0bd80bc9b55d3c73193564bc78
juanorosco59 <98904146+juanorosco59@users.noreply.github.com>
2025-11-01 10:56:24 -0500

    Update vite.config.js

--- web/js/vite.config.js
+++ web/js/vite.config.js
@@ -1,9 +1,17 @@
 // vite.config.js
 import { defineConfig } from 'vite';
 
+//export default defineConfig({
+//  root: 'web',
+//  server: {
+//    port: 8080, // puedes cambiarlo si quieres
+//  }
+//});
+
 export default defineConfig({
-  root: 'web',
-  server: {
-    port: 8080, // puedes cambiarlo si quieres
+  preview: {
+    allowedHosts: ['joyful-playfulness-production-1974.up.railway.app'],
+    host: '0.0.0.0',
+    port: process.env.PORT || 4173
   }
-});
+})
