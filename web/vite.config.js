2294fdc969dd405f02f8d9ab0b402049460d623e
juanorosco59 <juanorosco59@gmail.com>
2025-11-13 21:48:03 -0500

    deploy changes

--- web/vite.config.js
+++ web/vite.config.js
@@ -12,10 +12,21 @@ import { defineConfig } from 'vite';
 //
 //
 
-export default defineConfig({
-  preview: {
-    allowedHosts: ['childcaremap-capafrontend.up.railway.app'],
-    host: '0.0.0.0',
-    port: process.env.PORT || 4173
+//export default defineConfig({
+//  preview: {
+//    allowedHosts: ['childcaremap-capafrontend.up.railway.app'],
+//    host: '0.0.0.0',
+//    port: process.env.PORT || 4173
+//  }
+//})
+
+export default {
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        login: 'login.html',
+      }
+    }
   }
-})
+}
